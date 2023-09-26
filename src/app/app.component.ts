8761d7037a2d3dc04a5e5cdd8eee6c109033a0ba
Erivan-Leite <erivan.leite@academmico.ifpb.edu.br>
2023-09-26 19:48:39 -0300

    Exibindo o nome atraves de um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
+  aluno: String;
+
+  constructor() {
+    this.aluno = 'Erivan Soares Leite Junior'
+  }
 }
